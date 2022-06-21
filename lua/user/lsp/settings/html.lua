a833f88449544ca455048c159bd415718ec97a23
gbt24 <g1343537119@outlook.com>
2022-06-21 10:04:04 +0800

    添加html lsp补全支持

---
+++ lua/user/lsp/settings/html.lua
@@ -0,0 +1,13 @@
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.html.setup {
+  capabilities = capabilities,
+  configurationSection = { "html", "css", "javascript" },
+  embeddedLanguages = {
+    css = true,
+    javascript = true
+  },
+  provideFormatter = true
+}
