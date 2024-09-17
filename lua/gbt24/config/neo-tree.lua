03394cd05ff61c8ff04b5b12ad5146b73a869c8a
gbt24 <g1343537119@outlook.com>
2024-09-17 13:00:37 +0800

    2024.9.17-13:00

--- lua/gbt24/config/neo-tree.lua
+++ lua/gbt24/config/neo-tree.lua
@@ -1,214 +1,42 @@
-local function getTelescopeOpts(state, path)
-	return {
-		cwd = path,
-		search_dirs = { path },
-		attach_mappings = function(prompt_bufnr, _)
-			local actions = require("telescope.actions")
-			actions.select_default:replace(function()
-				actions.close(prompt_bufnr)
-				local action_state = require("telescope.actions.state")
-				local selection = action_state.get_selected_entry()
-				local filename = selection.filename
-				if filename == nil then
-					filename = selection[1]
-				end
-				-- any way to open the file without triggering auto-close event of neo-tree?
-				require("neo-tree.sources.filesystem").navigate(state, state.path, filename, function() end)
-			end)
-			return true
-		end,
-	}
+local M = {}
+
+-- Open file with system tools
+function M.system_open(state)
+    local node = state.tree:get_node()
+    local path = node:get_id()
+    vim.ui.open(path)
 end
 
-vim.keymap.set("n", "B", "<cmd>Neotree toggle<cr>", { desc = "Toggle Neotree" })
-local icons = require("gbt24.config.custom")
-vim.fn.sign_define("DiagnosticSignError", { text = icons.icons.diagnostic.error, texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = icons.icons.diagnostic.warn, texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = icons.icons.diagnostic.info, texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = icons.icons.diagnostic.hint, texthl = "DiagnosticSignHint" })
-local neotree = require("neo-tree")
-neotree.setup({
-	close_if_last_window = true,
-	enable_git_status = true,
-	enable_diagnostics = false,
-	sources = { "filesystem" },
-	source_selector = {
-		statusline = false,
-	},
-	open_files_do_not_replace_types = {
-		"terminal",
-		"trouble",
-		"qf",
-		"edgy",
-		"telescopeprompt",
-		"overseerlist",
-		"OverseerList",
-	},
-	commands = {
-		copy_selector = function(state)
-			local node = state.tree:get_node()
-			local filepath = node:get_id()
-			local filename = node.name
-			local modify = vim.fn.fnamemodify
+-- Try to move left in file tree smartly
+function M.smart_left(state)
+    local node = state.tree:get_node()
+    if node.type == 'directory' and node:is_expanded() then
+        if state.name == 'filesystem' then
+            require('neo-tree.sources.filesystem.commands').toggle_node(state)
+        else
+            require('neo-tree.sources.common.commands').toggle_node(state)
+        end
+    else
+        require('neo-tree.ui.renderer').focus_node(state, node:get_parent_id())
+    end
+end
 
-			local vals = {
-				["BASENAME"] = modify(filename, ":r"),
-				["EXTENSION"] = modify(filename, ":e"),
-				["FILENAME"] = filename,
-				["PATH (CWD)"] = modify(filepath, ":."),
-				["PATH (HOME)"] = modify(filepath, ":~"),
-				["PATH"] = filepath,
-				["URI"] = vim.uri_from_fname(filepath),
-			}
+-- Try to move right in file tree smartlly
+function M.smart_right(state)
+    local node = state.tree:get_node()
+    if node.type == 'directory' then
+        if not node:is_expanded() then
+            if state.name == 'filesystem' then
+                require('neo-tree.sources.filesystem.commands').toggle_node(state)
+            else
+                require('neo-tree.sources.common.commands').toggle_node(state)
+            end
+        elseif node:has_children() then
+            require('neo-tree.ui.renderer').focus_node(state, node:get_child_ids()[1])
+        end
+    elseif node.type == 'file' then
+        require('neo-tree.sources.common.commands').open(state, function() end)
+    end
+end
 
-			local options = vim.tbl_filter(function(val)
-				return vals[val] ~= ""
-			end, vim.tbl_keys(vals))
-			if vim.tbl_isempty(options) then
-				vim.notify("No values to copy", vim.log.levels.WARN)
-				return
-			end
-			table.sort(options)
-			vim.ui.select(options, {
-				prompt = "Choose to copy to clipboard:",
-				format_item = function(item)
-					return ("%s: %s"):format(item, vals[item])
-				end,
-			}, function(choice)
-				local result = vals[choice]
-				if result then
-					vim.notify(("Copied: `%s`"):format(result))
-					vim.fn.setreg("+", result)
-				end
-			end)
-		end,
-		telescope_find = function(state)
-			local node = state.tree:get_node()
-			local path = node:get_id()
-			require("telescope.builtin").find_files(getTelescopeOpts(state, path))
-		end,
-		telescope_grep = function(state)
-			local node = state.tree:get_node()
-			local path = node:get_id()
-			require("telescope.builtin").live_grep(getTelescopeOpts(state, path))
-		end,
-		open_externally = function(state)
-			local node = state.tree:get_node()
-			vim.notify("Opening " .. node.path)
-			if node.type == "directory" or node:has_children() then
-				if not node:is_expanded() then -- if unexpanded, expand
-					state.commands.toggle_node(state)
-				else -- if expanded and has children, seleect the next child
-					require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
-				end
-			elseif node.ext == "jpg" or node.ext == "png" or node.ext == "pdf" or node.ext == "svg" then
-				if vim.fn.has("mac") == 1 then
-					vim.fn.system("open " .. node.path)
-				else
-					vim.fn.jobstart(
-						"export DISPLAY=$(tmux show-env | sed -n 's/^DISPLAY=//p'); xdg-open "
-							.. node.path
-							.. " || evince "
-							.. node.path
-					)
-				end
-			elseif node.ext == "root" then
-				local tmux = require("tmux-awesome-manager.src.term")
-				tmux.run({
-					cmd = "bash -i -c 'root -l " .. node.path .. "'",
-					name = "Open ROOT File",
-					open_as = "pane",
-					orientation = "horizontal",
-					close_on_timer = 1,
-					visit_first_call = true, -- will not focus the pane
-					focus_when_call = true, -- Instead of focusing, will open a new pane in the same place as before
-				})()
-			else
-				state.commands.open(state)
-			end
-		end,
-		open_check_images = function(state)
-			local node = state.tree:get_node()
-			if node.ext == "jpg" or node.ext == "png" or node.ext == "pdf" then
-				local command = vim.fn.has("mac") == 1 and "open" or "xdg-open"
-				vim.notify("Open with " .. command)
-				vim.fn.jobstart(command .. " " .. node.path)
-			else
-				vim.cmd.edit(node.path)
-			end
-		end,
-	},
-	window = {
-		position = "left",
-		mappings = {
-			["t"] = false,
-			["tf"] = "telescope_find",
-			["tg"] = "telescope_grep",
-			["O"] = "open_externally",
-			["l"] = "open",
-			["h"] = "close_node",
-			["C"] = false,
-			["<c-s>"] = "open_split",
-			["<c-v>"] = "open_vsplit",
-			["D"] = "delete",
-			["d"] = false,
-			["s"] = false,
-			["A"] = false,
-			["zh"] = "toggle_hidden",
-			["z"] = false,
-			["Z"] = "close_all_nodes",
-			["H"] = "close_window",
-			["L"] = "close_window",
-			["Y"] = "copy_selector",
-			["<Space>"] = false,
-		},
-	},
-	filesystem = {
-		filtered_items = {
-			always_show = {
-				".config",
-				".gitignore",
-				"config.json",
-				"histo.root",
-				"tuple.root",
-				"yaml",
-				".schema.json",
-			},
-			hide_by_name = { "GeneratorLog.xml", "NewCatalog.xml" },
-			hide_by_pattern = {
-				"*.gif",
-				"*.pdf",
-				"*.png",
-				"*.sim",
-				"*.digi",
-				"*.mdf",
-				"*.dst",
-				"test_catalog-*.000000.xml",
-			},
-		},
-	},
-	event_handlers = {
-		{
-			event = "file_opened",
-			handler = function(_)
-				require("neo-tree.command").execute({ action = "close" })
-			end,
-		},
-	},
-	default_component_configs = {
-		git_status = {
-			symbols = {
-				-- Change type
-				added = "✚",
-				modified = "",
-				deleted = "✖",
-				renamed = "R", -- this can only be used in the git_status source
-				untracked = "?",
-				ignored = "",
-				unstaged = "󰄱",
-				staged = "",
-				conflict = "",
-			},
-		},
-	},
-})
+return M
