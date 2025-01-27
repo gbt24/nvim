9db5936101d71cbe7d732fa0ad4ac0d4d3ae1b4c
Gbt24 <g1343537119@outlook.com>
2025-01-27 11:34:08 +0800

    add

--- lua/custom/plugins/nvim-colorizer.lua
+++ lua/custom/plugins/nvim-colorizer.lua
@@ -1,6 +1,6 @@
 return {
   'NvChad/nvim-colorizer.lua',
-  ft = { 'lua', 'html', 'xml', 'python', 'lua' },
+  ft = { 'lua', 'html', 'xml', 'python', 'lua', 'cpp', 'c' },
   opts = {
     user_default_options = {
       names = false,
