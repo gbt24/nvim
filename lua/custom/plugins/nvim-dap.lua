6a5403c90d8bfbfaa8a45a843796ff1ba86cd06e
Gbt24 <g1343537119@outlook.com>
2025-02-16 22:19:58 +0800

    add dap

---
+++ lua/custom/plugins/nvim-dap.lua
@@ -0,0 +1,31 @@
+---@diagnostic disable: missing-parameter
+return {
+  {
+    'mfussenegger/nvim-dap',
+    event = 'VeryLazy',
+    config = function()
+      require 'custom.config.debugger'
+    end,
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap', 'theHamsta/nvim-dap-virtual-text', 'nvim-neotest/nvim-nio' },
+    config = function()
+      require('nvim-dap-virtual-text').setup() -- optional
+      require('noice').setup() -- optional
+      require 'custom.config.dapui'
+    end,
+  },
+  { -- python debugger
+    'mfussenegger/nvim-dap-python',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+      'rcarriga/nvim-dap-ui',
+    },
+    ft = { 'python' },
+    config = function()
+      local path = '~/.local/share/nvim/mason/packages/debugpy/venv/bin/python'
+      require('dap-python').setup(path)
+    end,
+  },
+}
